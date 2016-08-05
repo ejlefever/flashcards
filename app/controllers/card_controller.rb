67b49cfab4ec167f411c2849f8973c5c597193a3
Richie Ito & Walter Chabla <pair+ricito4.walterchabla@devbootcamp.com>
2016-08-05 14:50:37 -0400

    Add create deck and card methods

---
+++ app/controllers/card_controller.rb
@@ -0,0 +1,18 @@
+post '/cards/:id/create' do
+  @deck = Deck.find_by(id: params[:id])
+  attributes = params[:card].merge(deck_id: params[:id])
+  card = Card.new(attributes)
+
+  if @deck
+    if card.save
+      @deck
+      erb :'/cards/new'
+    else
+      @errors = card.errors.full_messeges
+      erb :'/cards/new'
+    end
+  else
+    @deck
+    erb :'/cards/new'
+  end
+end
